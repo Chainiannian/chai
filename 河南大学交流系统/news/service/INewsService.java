47e20d8de1cb32aa6a279c9f93cefe01ce135189
Chainiannian <34231441+Chainiannian@users.noreply.github.com>
2019-05-30 08:23:13 +0800

    Add files via upload

---
+++ 河南大学交流系统/news/service/INewsService.java
@@ -0,0 +1,22 @@
+package com.qhit.news.service;
+
+import java.util.List;
+import com.qhit.news.pojo.News;
+
+public interface INewsService {
+
+    boolean insert(Object object);
+
+    boolean  update(Object object);
+
+    boolean  updateSelective(Object object);
+
+    boolean delete(Object id);
+
+    List findAll();
+
+    News findById(Object id);
+
+    List<News> search(News news);
+
+}
